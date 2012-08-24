2f283dae36dee5da2c770e63a8193a567f4cab35
Michael Rooney <mrooney.github@rowk.com>
2012-08-23 23:44:54 -0400

    admin model register

--- project_name/models.py
+++ project_name/models.py
@@ -1,7 +1,8 @@
 from django.conf import settings
+from django.contrib import admin
 from django.db import models
 
 #class Foo(models.Model)
 #    name = models.CharField(max_length=255)
 
-
+#admin.site.register(Foo)
