9e28b8c3ffe035d6963fc6478a5200f7479d47db
Michael Rooney <mrooney.github@rowk.com>
2012-10-04 00:27:22 -0400

    minor adjustments based on recent usage

--- website/project_name/models.py
+++ website/project_name/models.py
@@ -1,8 +1,5 @@
 from django.conf import settings
-from django.contrib import admin
 from django.db import models
 
 #class Foo(models.Model)
 #    name = models.CharField(max_length=255)
-
-#admin.site.register(Foo)
