6809466e0d5ca2ae6ac5e712e086daa3614997b1
Michael Rooney <mrooney.github@rowk.com>
2012-10-12 12:38:58 -0400

    improve default tests to include a login/logout/signup 200 test, as well as an actual signup post and user round-trip

--- website/project_name/tests.py
+++ website/project_name/tests.py
@@ -2,13 +2,25 @@ from {{ project_name }}.test_helpers import ExtendedTestCase
 from {{ project_name }} import models
 
 class {{ project_name }}Tests(ExtendedTestCase):
-    def setUp(self):
-        self.post("/signup/", {"email": "test@example.com", "password": "foobar"})
-        self.user = models.User.objects.get(id=1)
+    def signup_user(self):
+        users = models.User.objects.count()
+        self.post("/signup/", {"email": "user{}@example.com".format(users), "password": "foobar", "deck": "0"})
+        return models.User.objects.get(id=users+1)
 
     def test_404(self):
         self.assertStatus(404, '/foobar/')
 
     def test_home(self):
         self.assertStatus(200, '/')
+
+    def test_login(self):
+        self.assertStatus(200, '/login/')
+
+    def test_logout(self):
+        self.assertStatus(200, '/logout/')
+
+    def test_signup(self):
+        self.assertStatus(200, '/signup/')
+        user = signup_user()
+        self.assertEqual(1, user.id)
 
