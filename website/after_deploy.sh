f346f945dbb12d131daf4ca66925584f2e0be45a
Michael Rooney <mrooney.github@rowk.com>
2012-11-13 16:03:25 -0500

    fix the greps and minimize output

--- website/after_deploy.sh
+++ website/after_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-python manage.py syncdb --migrate
+python manage.py syncdb --migrate --noinput | egrep -v "(Installed 0|Nothing to migrate|Loading initial data)"
 python manage.py test {{ project_name }} 2>&1
--- website/before_deploy.sh
+++ website/before_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-pip install -r ../requirements.txt | grep -v "(Requirement already satisfied|Cleaning up)"
+pip install -r ../requirements.txt | egrep -v "(Requirement already satisfied|Cleaning up)" || true
 python manage.py collectstatic --noinput
