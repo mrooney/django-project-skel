baebb95dfcd6b4e3fa4bc3db707a74e8d9df94ca
Michael Rooney <mrooney.github@rowk.com>
2013-01-11 11:54:16 -0500

    change loglevel so stdout, including console emails, go to gunicorn.log

--- website/project_name/settings.py
+++ website/project_name/settings.py
@@ -141,11 +141,6 @@ if not (DEBUG or TESTING):
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 SENTRY_DSN = open(os.path.join(WEBSITE_DIR, 'sentry.dsn')).read()
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -158,7 +153,7 @@ LOGGING = {
     'loggers': {
         'django.request': {
             'handlers': ['logfile'],
-            'level': 'ERROR',
+            'level': 'INFO',
             'propagate': True,
         },
     }
