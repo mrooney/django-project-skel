64914bbf4e9ca511a1ac598a803f3cf385bc21c8
Michael Rooney <mrooney.github@rowk.com>
2014-02-23 23:41:06 -0600

    email_re is gone in django 1.6

--- website/project_name/views.py
+++ website/project_name/views.py
@@ -5,16 +5,24 @@ from django.contrib.auth import authenticate, logout as logout_user, login as lo
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.core.urlresolvers import reverse
-from django.core.validators import email_re
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseServerError
 from django.shortcuts import redirect
 from django.template import RequestContext
 
 import cjson
 import uuid
+import re
 
 from {{project_name}} import models
 
+email_re = re.compile(
+     r"(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*"  # dot-atom
+     # quoted-string, see also http://tools.ietf.org/html/rfc2822#section-3.2.5
+     r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-\011\013\014\016-\177])*"'
+     r')@((?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)$)'  # domain
+     r'|\[(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\]$', re.IGNORECASE # literal form, ipv4 address (SMTP 4.1.3)
+)
+      
 def r2r(template, request, data=None):
     data = data or {}
     return render_to_response(template, data, context_instance=RequestContext(request))
